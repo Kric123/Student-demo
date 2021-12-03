a02e21b64cf614e613d9f8bf97503e1d1646616e
Kyle Riccardi <Kylericcardi99@gmail.com>
2021-12-02 20:53:22 -0500

    deploy to heroku

--- src/app/services/CourseService.ts
+++ src/app/services/CourseService.ts
@@ -3,6 +3,6 @@ import {Injectable} from "@angular/core";
 @Injectable()
 export class CourseService{
     findAllCourses = () =>
-    fetch('https://student-env.eba-rvpysy23.us-east-2.elasticbeanstalk.com/api/courses')
+    fetch('http://student-env.eba-rvpysy23.us-east-2.elasticbeanstalk.com/api/courses')
     .then(response => response.json())
 }
